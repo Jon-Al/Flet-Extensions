5d0845b479e4b1a81b5590819cf918910c6e3a16
Jonathan <yalexand@uwo.ca>
2025-03-03 14:00:45 -0500

    Minor bug fix

--- dark_theme_toggle.py
+++ dark_theme_toggle.py
@@ -1,14 +1,14 @@
 import flet as ft
 
 
-def theme_toggle_button(page: ft.Page, ft=None) -> ft.IconButton:
+def theme_toggle_button(page: ft.Page) -> ft.IconButton:
     """
     Creates a button that toggles between light and dark themes.
 
     :param page: The Flet page where the theme will be toggled.
     :return: An IconButton for theme switching.
     """
-    
+
     def on_click(e):
         if page.theme_mode == ft.ThemeMode.LIGHT:
             page.theme_mode = ft.ThemeMode.DARK
@@ -19,7 +19,7 @@ def theme_toggle_button(page: ft.Page, ft=None) -> ft.IconButton:
             dark_light_text.value = "Light theme"
             dark_light_icon.icon = ft.Icons.BRIGHTNESS_2
         page.update()
-    
+
     dark_light_text = ft.Text("Light theme")
     dark_light_icon = ft.IconButton(
         icon=ft.Icons.BRIGHTNESS_2_OUTLINED,

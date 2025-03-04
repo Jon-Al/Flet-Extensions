b9dbc0cb382e6403d2485a304e633ade0427de5c
Jonathan <yalexand@uwo.ca>
2025-03-04 16:34:33 -0500

    Bug fix

--- view_and_keyboard/template_view.py
+++ view_and_keyboard/template_view.py
@@ -1,14 +1,16 @@
 import flet as ft
 from typing_extensions import Sequence
 
-from view_and_keyboard.example import main
-
 
 class TemplateView(ft.View):
     def __init__(self, controls: Sequence[ft.Control] = None):
+        controls = list(controls) if controls else []
+        controls.append(ft.Container(ft.Text("I am a text"),
+                                     bgcolor=ft.Colors.random(),
+                                     width=100,
+                                     height=100))
         super().__init__(
             controls=controls,
             scroll=ft.ScrollMode.AUTO,
-            bgcolor=ft.Colors.random(),
             decoration=ft.BoxDecoration(border=ft.border.all(1, ft.Colors.OUTLINE))
         )
